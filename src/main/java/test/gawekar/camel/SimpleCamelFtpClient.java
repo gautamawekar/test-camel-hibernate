4e105fc718ab8fc02f89550876484bf97f5f6e21
Gautam Awekar <g.awekar@nature.com>
2013-04-25 10:56:26 +0100

    some more sampling

--- src/main/java/test/gawekar/camel/SimpleCamelFtpClient.java
+++ src/main/java/test/gawekar/camel/SimpleCamelFtpClient.java
@@ -1,53 +1,105 @@
 package test.gawekar.camel;
 
+
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.apache.camel.CamelContext;
 import org.apache.camel.Exchange;
+import org.apache.camel.ExchangeException;
 import org.apache.camel.Message;
 import org.apache.camel.Processor;
 import org.apache.camel.ProducerTemplate;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.properties.PropertiesComponent;
 import org.apache.camel.impl.DefaultCamelContext;
+import org.apache.camel.impl.SimpleRegistry;
+import org.apache.log4j.Logger;
+
 
 public class SimpleCamelFtpClient {
-	public static void main(String args[]) throws Exception{
-		if (args.length != 1){
-			System.out.println("error pattern should be ' ftp.username ftp.pattern a,b,c'");
+	private static Logger LOG = Logger.getLogger(SimpleCamelFtpClient.class);
+	
+	private static class LocalExceptionBean{
+		public void exceptionHandler(Exchange exchange, @ExchangeException Exception exception){
+			System.out.println("exceptiion: ");
+		}
+	}
+	
+	public static void main(final String args[]) throws Exception{
+		if (args.length != 2){
+			System.out.println("error pattern should be '{sleeptimeInSec} a,b,c'");
 			System.exit(0);
 		}
+		final String imageNames = args[1];
 		String confiFile = System.getProperty("config_file");
-		
+		final  SimpleDateFormat format = new SimpleDateFormat("HH:mm:ss:SS"); 
 		DefaultCamelContext ctx = new DefaultCamelContext();
 		PropertiesComponent pc = new PropertiesComponent();
 		pc.setLocation("file:" + confiFile);
 		ctx.addComponent("properties", pc);
+		SimpleRegistry registry = new SimpleRegistry();
+		ctx.setRegistry(registry);
+		registry.put("exceptionBean", new LocalExceptionBean());
+		
+		ctx.addRoutes(new RouteBuilder(){
+			@Override
+			public void configure() throws Exception {
+				from("timer://processingQueueReadyToDispatch?fixedRate=true&period={{npis.internal.dispatching.period}}")
+				.setBody(constant("" + imageNames))
+				.process(new Processor() {
+							public void process(Exchange exchange) throws Exception {
+								
+								System.out.println("\n\n about to make ftp calls: " + format.format(new Date()));
+							}
+		   		        })
+				.to("direct:a");
+			}
+		});
 		
 		ctx.addRoutes(new RouteBuilder() {
 		    public void configure() {
+		    	onException(Exception.class)
+		    	.handled(true)
+		    	.to("bean:exceptionBean");
+		    	
 		   		 from("direct:a")
-		   		 	.process(new Processor() {
-						public void process(Exchange exchange) throws Exception {
-							System.out.println(exchange.getIn().getBody());
-						}
-					})
 		            .split(body(String.class).tokenize(",")).parallelProcessing().threads(2)
 		                .to("direct:b");
 		    }
 		});
 		
 		ctx.addRoutes(new RouteBuilder() {
+			//.setHeader(Exchange.HTTP_PATH, constant("/preview/polopoly_fs/7.12698!/image/A-M%20Android%20Head.GIF_gen/derivatives/fullsize/A-M%20Android%20Head.GIF"))
 		    public void configure() {
 		   		        from("direct:b")
-						.setHeader(Exchange.HTTP_PATH, constant("/polopoly_fs/7.5072.1340638582!/image/1.10884.jpg_gen/derivatives/landscape_300/1.10884.jpg"))
-						//.setHeader(Exchange.HTTP_PATH, constant("/nature/index.html"))
-						//.setBody(constant(""))
+		   		        .process(new Processor() {
+							public void process(Exchange exchange) throws Exception {
+								System.out.println(Thread.currentThread().getId() +   " processing ==>   " + exchange.getIn().getBody());
+							}
+		   		        })
+		   		        //image pull & ftp
+		   		        .setHeader(Exchange.HTTP_PATH, simple("${properties:test.src.data.for.image}"))
 		   		        .setHeader("newFileName",simple("${body}"))
-						.to("http4://www.nature.com")
+						.to("http4://{{test.src.hostname.for.image}}")
 						.setHeader(Exchange.FILE_NAME, simple("${properties:test.fs.root}${header.newFileName}.gif"))
-						//.to("ftp://{{test.ftp.user}}@test-fs.nature.com/?password={{test.ftp.password}}&binary=true");
-						.to("ftp://{{test.ftp.user}}@test-fs.nature.com/?password={{test.ftp.password}}{{test.options}}");
-						//.to("ftp://gawekar@test-fs.nature.com/?password=gAw999em&binary=true");
-						//.to("ftp://npis@test-fs.nature.com/?password=FL500835e&binary=true");
+						.to("ftp://{{test.ftp.user}}@test-fs.nature.com/?password={{test.ftp.password}}{{test.options}}")
+						//text pull & push
+						.setBody(constant(""))
+						.setHeader(Exchange.HTTP_PATH, simple("${properties:test.src.data.for.text}"))
+						.to("http4://{{test.src.hostname.for.text}}")
+						.setHeader(Exchange.FILE_NAME, simple("${properties:test.fs.root}${header.newFileName}.txt"))
+						.to("ftp://{{test.ftp.user}}@test-fs.nature.com/?password={{test.ftp.password}}")
+						.process(new Processor() {
+							public void process(Exchange exchange) throws Exception {
+//								System.out.println(Thread.currentThread().getId() +   " finished processing. Going to sleep for a while 50 secs ==>   " + exchange.getIn().getHeader("newFileName") + "  time: " + format.format(new Date()));
+//								//throw new RuntimeException("Exception injected for thread: "  + Thread.currentThread().getId());
+//								Thread.sleep(50000);
+//								System.out.println(Thread.currentThread().getId() +   " woke up ==>   " + exchange.getIn().getHeader("newFileName") + "  time: " +  format.format(new Date()));
+								System.out.println("no sleeping=====> " +  format.format(new Date()));
+							}
+		   		        });
 //						.setHeader("CamelFileName",simple("${header.newFileName}.gif"))
 //						.to("file:target/");
 		    }
@@ -55,46 +107,14 @@ public class SimpleCamelFtpClient {
 		
 		try {
 			ctx.start();
-			ProducerTemplate template = ctx.createProducerTemplate();
-			template.sendBody("direct:a",args[0]);
+			System.out.println("Going to sleep for in sec? " + args[0]);
+			Thread.sleep(Integer.parseInt(args[0]) * 1000);
+//			ProducerTemplate template = ctx.createProducerTemplate();
+//			template.sendBody("direct:a",args[0]);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}finally{
-			System.clearProperty("test.ftp.user");
-			System.clearProperty("test.ftp.password");
 			ctx.shutdown();
 		}
 	}
-	
-	public static void main1(String[] args) { 
-        CamelContext context = new DefaultCamelContext(); 
-
-        try { 
-            ProducerTemplate template = context.createProducerTemplate(); 
-            context.start(); 
-            
-            Exchange exchange = template.request("http4://www.google.com/search", new Processor() { 
-            public void process(Exchange exchange) throws Exception { 
-            } 
-            }); 
-
-            if (null != exchange) { 
-                Message out = exchange.getOut(); 
-                int responseCode = out.getHeader(Exchange.HTTP_RESPONSE_CODE, Integer.class); 
-                System.out.println("Response: " + String.valueOf(responseCode)); 
-            } 
-        } catch (Exception ex) { 
-            //System.out.println("Exception: " + ex);
-        	ex.printStackTrace();
-        }finally{
-        	try {
-        		context.stop();
-			} catch (Exception e) {
-				throw new RuntimeException("error while stopping the context",e);
-			}
-        	
-        }
-
-        System.out.println("DONE!!"); 
-    }
 }
